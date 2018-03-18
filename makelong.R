f3dcf94442cbedc08713dc39e3f81d948ddd7d9a
reid <dreidpilch@gmail.com>
2018-03-17 23:37:13 -0400

    created d3 viz

--- makelong.R
+++ makelong.R
@@ -10,3 +10,15 @@ long = melt(d,id.vars=c('name','neighborhood','city'),variable.name='year',value
 long[is.na(stars),stars := "None"]
 
 write.csv(long,'longdata.csv',row.names=FALSE)
+
+# Count number of Michelin starred restaurants by city by year
+w1 <- long %>% filter(stars %in% c('1','2','3')) %>% group_by(city,year,stars) %>% summarize(ct=n())
+w2 <- dcast(w1,city + year ~ stars, value.var='ct',fill=0)
+names(w2)[3:5] <- c('onestar','twostars','threestars')
+write.csv(w2,'starcountcity.csv',row.names=FALSE)
+w2 <- data.table(w2)
+w2[,totalstars := onestar + twostars + threestars]
+
+w3 <- w2 %>% select(city,year,totalstars)
+w4 <- dcast(w3,year ~ city,value.var="totalstars",fill=0)
+write.csv(w4,'widedata.csv',row.names=FALSE)
